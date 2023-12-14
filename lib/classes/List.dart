4eb7ca7585a28ea767ca0a0b01c87f71967ab816
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2023-12-14 13:45:52 +0300

    Done

--- lib/classes/List.dart
+++ lib/classes/List.dart
@@ -1,50 +1,35 @@
-class Section{
-  String sname;
-
-  Section({
-    required this.sname,
-  });
-
-  List<Section> getSection{
-    List<Section> secteur = [];
-    secteur.add(Section(sname: "Gaming"));
-    secteur.add(Section(sname: "Dev"));
-    return secteur;
-  }
-}
-
-class Liste extends Section{
+class Liste{
   String lname;
+  String section;
   int element;
-  
   Liste({
     required this.lname,
     required this.element, 
-    required super.sname, 
+    required this.section, 
   });
 
   static List<Liste> getListe(){
     List<Liste> items = [];
-    items.add(Liste(lname: "",  element: 22));
-    items.add(Liste(lname: "", element: 8));
-    items.add(Liste(lname: "",  element: 9));
-    items.add(Liste(lname: "", element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "", element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 1));
-    items.add(Liste(lname: "",  element: 8));
-    items.add(Liste(lname: "",  element: 11));
-    items.add(Liste(lname: "",  element: 2));
-    items.add(Liste(lname: "",  element: 22));
-    items.add(Liste(lname: "",  element: 3));
-    items.add(Liste(lname: "",  element: 0));
-    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 22, section: ''));
+    items.add(Liste(lname: "", element: 8, section: ''));
+    items.add(Liste(lname: "",  element: 9, section: ''));
+    items.add(Liste(lname: "", element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "", element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 1, section: ''));
+    items.add(Liste(lname: "",  element: 8, section: ''));
+    items.add(Liste(lname: "",  element: 11, section: ''));
+    items.add(Liste(lname: "",  element: 2, section: ''));
+    items.add(Liste(lname: "",  element: 22, section: ''));
+    items.add(Liste(lname: "",  element: 3, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
+    items.add(Liste(lname: "",  element: 0, section: ''));
     return items;
   }
 }
