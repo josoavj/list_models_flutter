0268316b6d14fc0b894dda73c61915b41dbc43cd
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2023-12-14 13:27:28 +0300

    Others elements initialisation

--- lib/classes/List.dart
+++ lib/classes/List.dart
@@ -1,12 +1,36 @@
-class List{
+class Liste{
   String lname;
   String lSection;
   int element;
   
-  List({
+  Liste({
     required this.lname,
     required this.lSection,
     required this.element, 
   });
-  
+
+  static List<Liste> getListe(){
+    List<Liste> items = [];
+    items.add(Liste(lname: "", lSection: "", element: 22));
+    items.add(Liste(lname: "", lSection: "", element: 8));
+    items.add(Liste(lname: "", lSection: "", element: 9));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 1));
+    items.add(Liste(lname: "", lSection: "", element: 8));
+    items.add(Liste(lname: "", lSection: "", element: 11));
+    items.add(Liste(lname: "", lSection: "", element: 2));
+    items.add(Liste(lname: "", lSection: "", element: 22));
+    items.add(Liste(lname: "", lSection: "", element: 3));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "", lSection: "", element: 0));
+    return items;
+  }
 }
