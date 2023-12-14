a4b48a47902e427adf5d30a36056a0459c372a0a
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2023-12-14 13:15:14 +0300

    Class initialisation

---
+++ lib/classes/List.dart
@@ -0,0 +1,12 @@
+class List{
+  String lname;
+  String lSection;
+  int element;
+  
+  List({
+    required this.lname,
+    required this.lSection,
+    required this.element, 
+  });
+  
+}
---
+++ lib/classes/Persons.dart
@@ -0,0 +1,15 @@
+class Persons{
+  String name;
+  String fname;
+  String status;
+  int age;
+
+  Persons({
+    required this.name,
+    required this.fname,
+    required this.status,
+    required this.age
+  });
+
+  
+}
