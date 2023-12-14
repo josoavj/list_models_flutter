754d4bdaae734f1c88d8c62fdb0ce9a86ed84d2a
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2023-12-14 13:41:52 +0300

    Homepage initialisation

--- lib/classes/List.dart
+++ lib/classes/List.dart
@@ -1,36 +1,50 @@
-class Liste{
+class Section{
+  String sname;
+
+  Section({
+    required this.sname,
+  });
+
+  List<Section> getSection{
+    List<Section> secteur = [];
+    secteur.add(Section(sname: "Gaming"));
+    secteur.add(Section(sname: "Dev"));
+    return secteur;
+  }
+}
+
+class Liste extends Section{
   String lname;
-  String lSection;
   int element;
   
   Liste({
     required this.lname,
-    required this.lSection,
     required this.element, 
+    required super.sname, 
   });
 
   static List<Liste> getListe(){
     List<Liste> items = [];
-    items.add(Liste(lname: "", lSection: "", element: 22));
-    items.add(Liste(lname: "", lSection: "", element: 8));
-    items.add(Liste(lname: "", lSection: "", element: 9));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 1));
-    items.add(Liste(lname: "", lSection: "", element: 8));
-    items.add(Liste(lname: "", lSection: "", element: 11));
-    items.add(Liste(lname: "", lSection: "", element: 2));
-    items.add(Liste(lname: "", lSection: "", element: 22));
-    items.add(Liste(lname: "", lSection: "", element: 3));
-    items.add(Liste(lname: "", lSection: "", element: 0));
-    items.add(Liste(lname: "", lSection: "", element: 0));
+    items.add(Liste(lname: "",  element: 22));
+    items.add(Liste(lname: "", element: 8));
+    items.add(Liste(lname: "",  element: 9));
+    items.add(Liste(lname: "", element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "", element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 1));
+    items.add(Liste(lname: "",  element: 8));
+    items.add(Liste(lname: "",  element: 11));
+    items.add(Liste(lname: "",  element: 2));
+    items.add(Liste(lname: "",  element: 22));
+    items.add(Liste(lname: "",  element: 3));
+    items.add(Liste(lname: "",  element: 0));
+    items.add(Liste(lname: "",  element: 0));
     return items;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:list_models/home.dart';
+import 'package:list_models/Homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'List',
+      debugShowCheckedModeBanner: false,
+      title: 'List items',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage()
+      home: const HomePage(),
     );
   }
 }
