91a6fd6f964f72144d662cad55b6ff1c35c337f4
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2023-12-14 14:10:28 +0300

    List init

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:list_models/classes/Persons.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  
+  HomePage({super.key});
+  List<Persons> personsinfo = [];
+  void getPersons(){
+    personsinfo = Persons.getPersons();
+  }
+
   @override
   Widget build(BuildContext context){
+    getPersons();
     return Scaffold(
-      appBar: AppBar(title: const Text("Items", style: TextStyle(color: Colors.blueAccent),),),
-      body: Container(),
+      appBar: AppBar(title: const Text(
+        "Items", 
+        style: TextStyle(
+          color: Colors.blueAccent,
+          fontSize: 20,
+          fontWeight: FontWeight.w500,
+          ),),
+        centerTitle: true,),
+      body: Container(
+        
+
+      ),
     );
   }
   
