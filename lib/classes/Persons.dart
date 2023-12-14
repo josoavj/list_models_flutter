41d58e0167954dd49592486ddef7b961acb7d6ed
Josoa Vonjiniaina <josoavonjiniaina13@gmail.com>
2023-12-14 13:24:10 +0300

    Persons list init

--- lib/classes/Persons.dart
+++ lib/classes/Persons.dart
@@ -11,5 +11,12 @@ class Persons{
     required this.age
   });
 
-  
+  static List<Persons> getPersons(){
+    List<Persons> personsinfo = [];
+    personsinfo.add(Persons(name: "Yegar", fname: "Joshua", status: "Single", age: 20));
+    personsinfo.add(Persons(name: "Ackerman", fname: "Livaï", status: "Single", age: 24));
+    personsinfo.add(Persons(name: "Ackermann", fname: "Mikasa", status: "Single", age: 21));
+    personsinfo.add(Persons(name: "Yager", fname: "Zeke", status: "Single", age: 17));
+    return personsinfo;
+  } 
 }
